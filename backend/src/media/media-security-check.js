e3aad9f40304f78f60bf6f7427b30e9c68b03fa8
nosato0519 <sato620519@gmail.com>
2026-08-26 15:11:32 +0900

    Milestone 271: validate protected media security configuration

---
+++ backend/src/media/media-security-check.js
@@ -0,0 +1,8 @@
+export function validateMediaSecurityConfig(env = process.env) {
+  if (!env.MEDIA_URL_SECRET) throw new Error('media_url_secret_missing');
+  if (!env.MEDIA_STORAGE_DIR) throw new Error('media_storage_dir_missing');
+  if (env.MEDIA_STORAGE_PROVIDER && env.MEDIA_STORAGE_PROVIDER !== 'local') {
+    throw new Error('media_storage_provider_unsupported');
+  }
+  return true;
+}
