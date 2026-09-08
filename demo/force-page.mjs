615e5795dc2de4adb3700d515c5aed3a3e693f59
nosato0519 <sato620519@gmail.com>
2026-09-08 13:30:14 +0900

    Expose demo library route

--- demo/force-page.mjs
+++ demo/force-page.mjs
@@ -43,7 +43,7 @@ function finalize(html) {
 }
 
 function servePage(pathname, res) {
-  const allowed = new Set(['/pages/video-list.html','/pages/product-detail.html','/pages/checkout.html']);
+  const allowed = new Set(['/pages/video-list.html','/pages/product-detail.html','/pages/checkout.html','/pages/library.html']);
   if (!allowed.has(pathname)) return false;
   readFile(join(ROOT, pathname.slice(1)), 'utf8').then(html => {
     const body = Buffer.from(html, 'utf8');
